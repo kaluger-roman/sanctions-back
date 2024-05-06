c2f4b389ac9d61fe1c1424f6fb227d14c3301415
Roman Kaluger <r.kaluger@sftpro.ru>
2024-05-06 23:01:44 +0300

    init

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    TRANSLATE_HOST: string;
-    TTS_HOST: string;
-    FFMPEG_HOST: string;
-    AUTH_HOST: string;
     SERVER_PORT: string;
   }
 }

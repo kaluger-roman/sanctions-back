0866159fde55e99a417fe677696e6f292a540a4c
Roman Kaluger <r.kaluger@sftpro.info>
2024-12-17 20:58:47 +0300

    expand desc

--- front/src/pages/search-app/components/data-row.tsx
+++ front/src/pages/search-app/components/data-row.tsx
@@ -1,13 +1,7 @@
 import { TableRow, TableCell, Link } from "@mui/material";
-import { useEffect, useRef } from "react";
 import { Sanction } from "shared/sanctions";
 import { theme } from "shared/theme";
-
-const colorHighlight = (window as any).Highlight
-  ? new (window as any).Highlight()
-  : null;
-
-if (colorHighlight) (CSS as any).highlights.set(`higlight`, colorHighlight);
+import { DescriptionBlock } from "./description-block";
 
 export const DataRow = ({
   code,
@@ -16,59 +10,21 @@ export const DataRow = ({
   description,
   matchedWords,
   isLast,
-}: Sanction & { isLast: boolean }) => {
-  const highlightRef = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    const text = highlightRef.current?.firstChild;
-    if (!(CSS as any).highlights || !text || !matchedWords) return;
-
-    for (let word of matchedWords) {
-      const regex = new RegExp(
-        word.replaceAll(/[#-.]|[[-^]|[?|{}]/g, "\\$&"),
-        "g",
-      );
-      const matches = Array.from(description.matchAll(regex));
-
-      for (let match of matches) {
-        const range = new Range();
-
-        range.setStart(text, match.index as number);
-        range.setEnd(text, (match.index as number) + match[0].length);
-        colorHighlight.add(range);
-      }
-    }
-
-    return () => {
-      colorHighlight.clear();
-    };
-  }, [description, matchedWords]);
-
-  return (
-    <TableRow
-      sx={{
-        borderBottomColor: isLast
-          ? "inherit"
-          : `${theme.palette.grey[300]} !important`,
-      }}
-    >
-      <TableCell sx={{ verticalAlign: "top" }}>{code}</TableCell>
-      <TableCell sx={{ verticalAlign: "top" }}>
-        <Link href="#">{sourceDocument}</Link>
-      </TableCell>
-      <TableCell sx={{ verticalAlign: "top" }}>{restriction}</TableCell>
-      <TableCell
-        ref={highlightRef}
-        sx={{
-          verticalAlign: "top",
-          whiteSpace: "pre-wrap",
-          "::highlight(higlight)": {
-            background: theme.palette.primary.light,
-          },
-        }}
-      >
-        {description}
-      </TableCell>
-    </TableRow>
-  );
-};
+}: Sanction & { isLast: boolean }) => (
+  <TableRow
+    sx={{
+      borderBottomColor: isLast
+        ? "inherit"
+        : `${theme.palette.grey[300]} !important`,
+    }}
+  >
+    <TableCell sx={{ verticalAlign: "top" }}>{code}</TableCell>
+    <TableCell sx={{ verticalAlign: "top" }}>
+      <Link href="#">{sourceDocument}</Link>
+    </TableCell>
+    <TableCell sx={{ verticalAlign: "top" }}>{restriction}</TableCell>
+    <TableCell>
+      <DescriptionBlock matchedWords={matchedWords} description={description} />
+    </TableCell>
+  </TableRow>
+);
---
+++ front/src/pages/search-app/components/description-block.tsx
@@ -0,0 +1,107 @@
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUp from "@mui/icons-material/KeyboardArrowUp";
+import { IconButton, Box, Collapse, Typography } from "@mui/material";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { theme } from "shared/theme";
+
+const colorHighlight = (window as any).Highlight
+  ? new (window as any).Highlight()
+  : null;
+
+if (colorHighlight) (CSS as any).highlights.set(`higlight`, colorHighlight);
+
+export const DescriptionBlock = ({
+  description,
+  matchedWords,
+}: {
+  description: string;
+  matchedWords: Array<string>;
+}) => {
+  const [expanded, setExpanded] = useState(false);
+  const [isOverflow, setIsOverflow] = useState<boolean>(true);
+  const highlightRef = useRef<HTMLSpanElement>(null);
+  const maxHeight = 198;
+
+  const toggleExpand = () => setExpanded((prev) => !prev);
+
+  useLayoutEffect(() => {
+    if (!highlightRef.current) return;
+
+    setIsOverflow(highlightRef.current.scrollHeight > maxHeight);
+  }, [highlightRef]);
+
+  useEffect(() => {
+    const text = highlightRef.current?.firstChild;
+    if (!(CSS as any).highlights || !text || !matchedWords) return;
+
+    for (let word of matchedWords) {
+      const regex = new RegExp(
+        word.replaceAll(/[#-.]|[[-^]|[?|{}]/g, "\\$&"),
+        "g",
+      );
+      const matches = Array.from(description.matchAll(regex));
+
+      for (let match of matches) {
+        const range = new Range();
+
+        range.setStart(text, match.index as number);
+        range.setEnd(text, (match.index as number) + match[0].length);
+        colorHighlight.add(range);
+      }
+    }
+
+    return () => {
+      colorHighlight.clear();
+    };
+  }, [description, matchedWords]);
+
+  return (
+    <Box sx={{ position: "relative", overflow: "hidden" }}>
+      <Collapse
+        in={!isOverflow || expanded}
+        collapsedSize={isOverflow ? maxHeight : undefined}
+        timeout={300}
+      >
+        <Typography
+          ref={highlightRef}
+          variant="body2"
+          sx={{
+            mr: 1,
+            verticalAlign: "top",
+            whiteSpace: "pre-wrap",
+            "::highlight(higlight)": {
+              background: theme.palette.primary.light,
+            },
+          }}
+        >
+          {description}
+        </Typography>
+      </Collapse>
+      {isOverflow && (
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            background: expanded
+              ? undefined
+              : "linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent)",
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "100%",
+            pointerEvents: "none",
+          }}
+        >
+          <IconButton
+            onClick={toggleExpand}
+            color="primary"
+            size="small"
+            sx={{ pointerEvents: "auto", padding: 0 }}
+          >
+            {expanded ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          </IconButton>
+        </Box>
+      )}
+    </Box>
+  );
+};

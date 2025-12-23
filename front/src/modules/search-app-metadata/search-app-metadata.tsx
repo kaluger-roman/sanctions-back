5859e2226e82385534c4a65cb070b85a7b802d30
Roman Kaluger <r.kaluger@sftpro.info>
2025-12-23 18:03:27 +0600

    feat: update sanctions dates and add new entries for ЕС, Великобритания, and США

--- front/src/modules/search-app-metadata/search-app-metadata.tsx
+++ front/src/modules/search-app-metadata/search-app-metadata.tsx
@@ -37,12 +37,12 @@ const ACTUAL_DATES: Array<{
   date: string;
   flag: React.ReactElement;
 }> = [
-  { country: "ЕС", date: "23.10.2025", flag: <EU /> },
-  { country: "Великобритания", date: "24.04.2025", flag: <GB /> },
-  { country: "США", date: "30.09.2025", flag: <US /> },
+  { country: "ЕС", date: "15.11.2025", flag: <EU /> },
+  { country: "Великобритания", date: "16.12.2025", flag: <GB /> },
+  { country: "США", date: "05.12.2025", flag: <US /> },
   { country: "Южная Корея", date: "09.09.2024", flag: <KR /> },
   { country: "Австралия", date: "08.04.2024", flag: <AU /> },
-  { country: "Китай", date: "01.12.2024", flag: <CN /> },
+  { country: "Китай", date: "12.12.2025", flag: <CN /> },
   { country: "Япония", date: "09.10.2025", flag: <JP /> },
   { country: "Тайвань", date: "05.11.2024", flag: <TW /> },
   { country: "Швейцария", date: "30.10.2025", flag: <CH /> },
